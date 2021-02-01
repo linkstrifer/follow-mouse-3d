4ef18fd35de95ba4be344d4387436b036a071adb
Link Strifer <linkstrifer@outlook.com>
2021-02-01 17:20:47 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const container = document.documentElement;
+
+document.addEventListener("mousemove", ({ x, y }) => {
+  const { height, width } = container.getBoundingClientRect();
+  const degUnit = {
+    x: width / (2 * 80),
+    y: height / (2 * 80)
+  };
+  document.querySelectorAll(".box").forEach((box) => {
+    setCoordinates({
+      element: box,
+      mouse: { x, y },
+      degUnit
+    });
+  });
+});
+
+function setLimit(num, min, max) {
+  const MIN = min || -80;
+  const MAX = max || 80;
+  const parsed = parseFloat(num);
+
+  return Math.min(Math.max(parsed, MIN), MAX);
+}
+
+function setCoordinates({ element, degUnit, mouse: { x, y } }) {
+  const { left, top, height, width } = element.getBoundingClientRect();
+  const center = {
+    x: left + width / 2,
+    y: top + height / 2
+  };
+
+  const coordinates = {
+    x: setLimit((x - center.x) / degUnit.x),
+    y: setLimit((y - center.y) / degUnit.y)
+  };
+
+  element.style.setProperty("--y", `${coordinates.x}deg`);
+  element.style.setProperty("--x", `${coordinates.y * -1}deg`);
+}
